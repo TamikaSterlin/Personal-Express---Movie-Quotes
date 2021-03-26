db21374fdd48212d19beeb29bc1be4f1a90a0f90
tamika sterlin <sterlintamika@gmail.com>
2021-03-26 18:50:15 -0400

    personal express app

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const PORT = process.env.PORT || 3000
+const url = process.env.DB_URL
+const urlName = process.env.DB_NAME
+
+var db, collection;
+
+
+app.listen(PORT, () => {
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        db = client.db(urlName);
+        console.log("Connected to `" + urlName + "`!");
+    });
+});
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  db.collection('messages').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {messages: result})
+  })
+})
+
+app.post('/messages', (req, res) => {
+  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database', req.body, result)
+    res.redirect('/')
+  })
+})
+
+app.put('/messages', (req, res) => {
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbUp + 1,
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.put('/messages/thumbDown', (req, res) => {
+  console.log(req.body);
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbDown:req.body.thumbDown + 1
+      }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+
+app.delete('/messages', (req, res) => {
+  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('Message deleted!')
+  })
+})
